592fc0240ac13da79515bd5d3b0a10339f2cd484
thistle <thxstle.branch@gmail.com>
2026-02-11 11:16:31 +0100

    init

---
+++ src/main/java/com/example/matador/MatadorApplication.java
@@ -0,0 +1,13 @@
+package com.example.matador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatadorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatadorApplication.class, args);
+    }
+
+}
