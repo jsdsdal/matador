ed5680e6aff58f9e1db1cdd301ddaa2d00eece5e
johanoliverlarsen <johanoliverlarsen@gmail.com>
2026-02-12 14:39:50 +0100

    tilføjelse af html i tags.html + gettags i controller

--- src/main/java/com/example/matador/controller/TouristController.java
+++ src/main/java/com/example/matador/controller/TouristController.java
@@ -1,52 +1,28 @@
 package com.example.matador.controller;
 
-import com.example.matador.model.TouristAttraction;
 import com.example.matador.service.TouristService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import static jdk.internal.org.jline.reader.impl.LineReaderImpl.CompletionType.List;
+
 
 public class TouristController {
 
     private final TouristService service;
+
     public TouristController(TouristService service) {
         this.service = service;
     }
 
-    @GetMapping()
-    public ResponseEntity<List<TouristAttraction>> getTouristAttractions() {
-        List<TouristAttraction> touristAttractions = service.getTouristAttractions();
-        return new ResponseEntity<>(touristAttractions, HttpStatus.OK);
+    @GetMapping("/{name}/tags")
+    public String getTagsForTouristAttraction(Model model){
+        model.addAttribute("tags", )
+
+        return "tags";
     }
 
-    @GetMapping("{name}")
-    public ResponseEntity<TouristAttraction> getTouristAttractionByName(@PathVariable String name) {
-        TouristAttraction touristAttraction = service.getTouristAttractionByName(name);
-        if (touristAttraction==null) {
-            return new ResponseEntity<>(touristAttraction, HttpStatus.NOT_FOUND);
-        } else {
-            return new ResponseEntity<>(touristAttraction, HttpStatus.OK);
-        }
-    }
 
-    @PostMapping("add")
-    public ResponseEntity<TouristAttraction> addTouristAttraction(@RequestBody TouristAttraction touristAttraction){
-        TouristAttraction addedTouristAttraction = service.addTouristAttraction(touristAttraction);
-        return ResponseEntity.ok(touristAttraction);
-    }
 
-    @PostMapping("{name}")
-    public ResponseEntity<TouristAttraction> deleteByName(@PathVariable String name) {
-        TouristAttraction touristAttraction = service.deleteByName(name);
-        if (touristAttraction==null) {
-            return new ResponseEntity<>(touristAttraction, HttpStatus.NOT_FOUND);
-        } else {
-            return new ResponseEntity<>(touristAttraction, HttpStatus.OK);
-        }
-    }
 }
