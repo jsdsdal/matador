7d457189d21b91be972e0e19df26ff59d8fcdbe7
johanoliverlarsen <johanoliverlarsen@gmail.com>
2026-02-12 15:05:58 +0100

    Test

--- src/main/java/com/example/matador/controller/TouristController.java
+++ src/main/java/com/example/matador/controller/TouristController.java
@@ -1,12 +1,13 @@
 package com.example.matador.controller;
 
+import com.example.matador.model.Tags;
+import com.example.matador.model.TouristAttraction;
 import com.example.matador.service.TouristService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 import static jdk.internal.org.jline.reader.impl.LineReaderImpl.CompletionType.List;
 
 public class TouristController {
