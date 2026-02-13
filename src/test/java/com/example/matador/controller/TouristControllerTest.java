b79936096ed53ce74eb697533a24d5eb85edbd69
Nelsoon-ds <davidgaming108@gmail.com>
2026-02-13 13:36:46 +0100

    Lavet første test! getAllAttractions

--- src/test/java/com/example/matador/controller/TouristControllerTest.java
+++ src/test/java/com/example/matador/controller/TouristControllerTest.java
@@ -1,12 +1,32 @@
 package com.example.matador.controller;
 
+import com.example.matador.repository.TouristRepository;
+import com.example.matador.service.TouristService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@WebMvcTest(TouristController.class)
 class TouristControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockitoBean
+    TouristRepository repository;
+
+    @MockitoBean
+    TouristService service;
+
+
 
     @BeforeEach
     void setUp() {
@@ -17,7 +37,10 @@ class TouristControllerTest {
     }
 
     @Test
-    void getAllAttractions() {
+    void getAllAttractions() throws Exception {
+        mockMvc.perform(get("/attractions"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("attractionList"));
     }
 
     @Test
