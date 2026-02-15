cc6154cb4a7460a9303d3217c0758d65bf6c027d
Nelsoon-ds <davidgaming108@gmail.com>
2026-02-15 17:14:33 +0100

    Nu burde det faile

--- src/test/java/com/example/matador/controller/TouristControllerTest.java
+++ src/test/java/com/example/matador/controller/TouristControllerTest.java
@@ -1,11 +1,28 @@
 package com.example.matador.controller;
 
+import com.example.matador.model.TouristAttraction;
+import com.example.matador.service.TouristService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 class TouristControllerTest {
 
+
+    @Autowired
+    MockMvc mockMvc;
+    @MockitoBean
+    TouristService
+
+
     @Test
     void getAllAttractions() {
+        MockMvc
     }
 
     @Test
