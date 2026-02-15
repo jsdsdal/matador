bb7a60c8ebc08d7c0b9cc8396ef33b96afcdd005
Nelsoon-ds <davidgaming108@gmail.com>
2026-02-15 17:12:07 +0100

    Tester vi lige git hooks

---
+++ src/test/java/com/example/matador/controller/TouristControllerTest.java
@@ -0,0 +1,38 @@
+package com.example.matador.controller;
+
+import org.junit.jupiter.api.Test;
+
+class TouristControllerTest {
+
+    @Test
+    void getAllAttractions() {
+    }
+
+    @Test
+    void showTouristAttractionCreationForm() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void getTagsForTouristAttraction() {
+    }
+
+    @Test
+    void deleteByName() {
+    }
+
+    @Test
+    void getDeleteByName() {
+    }
+
+    @Test
+    void getUpdateAttraction() {
+    }
+
+    @Test
+    void updateAttraction() {
+    }
+}
